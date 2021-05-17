2837fd99be287f0d7c86623cd6b0d9110ed749d0
mskalandunas <mskalandunas@gmail.com>
2021-05-16 23:18:17 -0700

    clean up browser/* spacing

--- browser/api/registry.js
+++ browser/api/registry.js
@@ -1,7 +1,7 @@
 import { wrapURLs } from '../utils';
 
 export const batchFetchDecks = ({ url, success, fail }) => {
-    return fetch(url).then(response => response.json()).then(data => {
-        return Promise.all(wrapURLs(data.decks)).then(success, fail).catch(fail);
-    });
+  return fetch(url).then(response => response.json()).then(data => {
+    return Promise.all(wrapURLs(data.decks)).then(success, fail).catch(fail);
+  });
 };
--- browser/utils/react.js
+++ browser/utils/react.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
 export const applyPropsToChildren = props => child =>
-    React.isValidElement(child) ? React.cloneElement(child, props) : child;
+  React.isValidElement(child) ? React.cloneElement(child, props) : child;
